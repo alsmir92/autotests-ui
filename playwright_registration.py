51c3623ba4383450f3492703f8d56c55e214fc21
alsmir92 <alexsmirdocs@gmail.com>
2025-09-02 12:53:41 +0300

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,29 +1,38 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright
 
-with (sync_playwright() as playwright):
+with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
     page.goto(
         "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    email_input = page.get_by_test_id("registration-form-email-input").locator(
+    email_input = page.get_by_test_id('registration-form-email-input').locator(
         'input')
-    email_input.fill("user.name@gmail.com")
+    email_input.fill('user.name@gmail.com')
 
     username_input = page.get_by_test_id(
-        "registration-form-username-input").locator(
-        'input')
+        'registration-form-username-input').locator('input')
     username_input.fill('username')
 
     password_input = page.get_by_test_id(
-        "registration-form-password-input").locator(
-        'input')
+        'registration-form-password-input').locator('input')
     password_input.fill('password')
 
     registration_button = page.get_by_test_id(
-        "registration-page-registration-button")
+        'registration-page-registration-button')
     registration_button.click()
 
-    dashboard_link = page.get_by_test_id("dashboard-toolbar-title-text")
-    expect(dashboard_link).to_have_text('Dashboard')
+    # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
+    context.storage_state(path="browser-state.json")
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+
+    page.goto(
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    page.wait_for_timeout(50000)
